b9fea023b7fe89047a29cd8e98eec4d5cb6bcf1e
EthanJ2000 <52162288+EthanJ2000@users.noreply.github.com>
2019-07-29 14:10:09 +0200

    Add files via upload

--- public/databasesave.js
+++ public/databasesave.js
@@ -1,20 +1,20 @@
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAH4-89xzGhI1q1lmtKDEBIa5-Ys4eMeBw",
-    authDomain: "form-658b2.firebaseapp.com",
-    databaseURL: "https://form-658b2.firebaseio.com",
-    projectId: "form-658b2",
-    storageBucket: "form-658b2.appspot.com",
-    messagingSenderId: "1051013261903",
-    appId: "1:1051013261903:web:5e7991d3774a5300"
+  apiKey: "AIzaSyAH4-89xzGhI1q1lmtKDEBIa5-Ys4eMeBw",
+  authDomain: "form-658b2.firebaseapp.com",
+  databaseURL: "https://form-658b2.firebaseio.com",
+  projectId: "form-658b2",
+  storageBucket: "form-658b2.appspot.com",
+  messagingSenderId: "1051013261903",
+  appId: "1:1051013261903:web:5e7991d3774a5300"
   };
 
 
+  grecaptcha.execute();
+  grecaptcha.render();
 
   firebase.initializeApp(firebaseConfig);
 
-  grecaptcha.execute();
-  grecaptcha.render();
 
 
 
