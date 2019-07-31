6274205fe9f6d4e6d2cb35430c2c0728fb485c91
EthanJ2000 <52162288+EthanJ2000@users.noreply.github.com>
2019-07-31 21:49:51 +0200

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,57 +1,82 @@
-'use strict';
+
 
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+// var admin = require('firebase-admin');
+// const express = require('express');
+//Email Start
 const nodemailer = require('nodemailer');
+// const app = express()
 
-//to make it work you need gmail account
-const gmailEmail = functions.config().gmail.login;
-const gmailPassword = functions.config().gmail.pass;
+// var emailRecepient;
+// var userName;
 
-admin.initializeApp();
+// const gmailEmail = functions.config().gmail.email;
+// const gmailPassword = functions.config().gmail.password;
+ 
 
-//creating function for sending emails
-var goMail = function (message) {
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  // host: 'smtp.gmail.com',
+  auth: {
+    user: 'ethanluke.portfolio@gmail.com', 
+    pass: 'hunghom2011'
+  }
+  });
 
-//transporter is a way to send your emails
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: ethan.jansen@younglings.africa,
-            pass: gmailPassword
-        }
+exports.sendEmails1 = functions.database.ref('/messages/{name}').onCreate((snap, context) => {
+
+  const vals = snap.val()
+  var name = vals.Name;
+  var email = vals.Email;
+  var message = vals.Message;
+
+  var mailOptions = {
+    from: 'ethanluke.portfolio@gmail.com',
+    to: 'ethanluke.portfolio@gmail.com',
+    subject: 'New Form Submitted',
+    html:`<h1>New Form Submitted:</h2><p>From: ${name}, ${email}</p> <p>Message: ${message}</p>`
+    // html:" <h2><p>${Name}, ${Email}</p></h2><h3>${message}</h3>"
+  };
+  
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      }
+      else {
+        console.log('Email sent: ' + info.response);
+      }
     });
 
-    // setup email data with unicode symbols
-    //this is how your email are going to look like
-    const mailOptions = {
-        from: gmailEmail, // sender address
-        to: 'salesdepartment@youcompany.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: '!' + message, // plain text body
-        html: '!' + message // html body
-    };
-
-    //this is callback function to return status to firebase console
-    const getDeliveryStatus = function (error, info) {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    };
-
-    //call of this function send an email, and return status
-    transporter.sendMail(mailOptions, getDeliveryStatus);
-};
-
-//.onDataAdded is watches for changes in database
-exports.onDataAdded = functions.database.ref('/emails/{sessionId}').onCreate(function (snap, context) {
-
-    //here we catch a new data, added to firebase database, it stored in a snap variable
-    const createdData = snap.val();
-    var text = createdData.mail;
-
-    //here we send new data using function for sending emails
-    goMail(text);
+  return null;
 });
+
+//Email end
+
+//Recaptcha start
+
+const rp = require('request-promise')
+
+exports.checkRecaptcha = functions.https.onRequest((req, res) => {
+    const response = req.query.response
+    console.log("recaptcha response", response)
+    rp({
+        uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
+        method: 'POST',
+        formData: {
+            secret: '6LealK8UAAAAAG_PYZx00-YdGE5XUFsmqHIsCmGU',
+            response: response
+        },
+        json: true
+    }).then(result => {
+        console.log("recaptcha result", result)
+        if (result.success) {
+            res.send("You're good to go, human.")
+        }
+        else {
+            res.send("Recaptcha verification failed. Are you a robot?")
+        }
+    }).catch(reason => {
+        console.log("Recaptcha request failure", reason)
+        res.send("Recaptcha request failed.")
+    })
+})
